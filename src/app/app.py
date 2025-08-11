e64794b3f3dcf6522c9fee00c30c110123c630d5
Aldo Camacho <me@aldocamacho.com>
2025-08-11 14:07:42 -0700

    add button to search for cities

--- src/app/app.py
+++ src/app/app.py
@@ -1,6 +1,6 @@
 from textual.app import App, ComposeResult
-from textual.containers import VerticalScroll
-from textual.widgets import Footer, Header
+from textual.containers import VerticalScroll, Container
+from textual.widgets import Button, Footer, Header
 
 from app.city_weather_card import CityWeatherCard
 from app.city_search import CitySearch
@@ -14,8 +14,7 @@ class WeatherApp(App):
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
-        yield WeatherCardContainer(id="card-container")
-        yield CitySearch(id="city-search")
+        yield WeatherCardContainer()
 
     def on_city_search_cities_selected(self, message: CitySearch.CitiesSelected):
         for city in message.cities:
@@ -24,17 +23,26 @@ class WeatherApp(App):
             )
 
     def action_search_city(self):
-        self.mount(CitySearch(id="city-search"))
+        if not self.query("#city-search"):
+            self.mount(CitySearch(id="city-search"))
 
-    def action_print_children(self):
-        print("children: ")
-        print(self.children)
+    def on_button_pressed(self, message: Button.Pressed):
+        if message.button.id == "btn-add-card":
+            self.action_search_city()
 
 
-class WeatherCardContainer(VerticalScroll):
+class WeatherCardContainer(VerticalScroll, can_focus=False):
     BINDINGS = [("t", "move_card_top", "Move to the top")]
+
+    def compose(self):
+        yield Container(id="card-container")
+        yield Button("+", "success", id="btn-add-card")
 
     def action_move_card_top(self):
         focused = self.app.focused
-        if focused and "city-weather" in focused.classes:
+        if (
+            focused
+            and "city-weather" in focused.classes
+            and focused.id != "btn-add-card"
+        ):
             self.move_child(focused, before=0)
